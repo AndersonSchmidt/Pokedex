4cdc5824c4cfbecaa6980af01c3af6c199aaaeb4
Anderson Schmidt <schmidtanderson@hotmail.com>
2020-06-06 08:56:48 -0300

    Configuring Navigator

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,37 +1,12 @@
-import {Platform} from 'react-native';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 
-import Colors from '../constants/Colors';
 import PokemonsScreen from '../screens/pokemons/PokemonsScreen';
 import PokemonScreen from '../screens/pokemons/PokemonScreen';
 
-const defaultStackNavOptions = {
-  defaultNavigationOptions: {
-    headerStyle: {
-      //   backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
-    },
-    headerTitleStyle: {
-      //   fontFamily: 'open-sans-bold',
-    },
-    headerBackTitleStyle: {
-      //   fontFamily: 'open-sans',
-    },
-    // headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
-    // headerTitle: 'A Screen',
-  },
-};
-
-const PokemonNavigator = createStackNavigator(
-  {
-    Pokemons: PokemonsScreen,
-    Pokemon: PokemonScreen,
-  },
-  {
-    defaultNavigationOptions: {
-      defaultStackNavOptions,
-    },
-  },
-);
+const PokemonNavigator = createStackNavigator({
+  Pokemons: PokemonsScreen,
+  Pokemon: PokemonScreen,
+});
 
 export default createAppContainer(PokemonNavigator);
